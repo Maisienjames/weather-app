2f5e149a50bd9c43328a967b33a85868ab8256aa
maisienjames <maisienjames@gmail.com>
2022-07-01 20:59:38 +0100

    Added location details

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
+import "../styles/App.css";
+
+function App({ location }) {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
